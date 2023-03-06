2d106fc99666d7238edc5851cbf1daada31d87d6
andyEmilowa <andyowaa@gmail.com>
2023-03-06 09:20:16 +0200

    akepnah inwaita :)

--- SkeletListManipulation/ListManipulation/Program.cs
+++ SkeletListManipulation/ListManipulation/Program.cs
@@ -41,7 +41,7 @@ namespace ListManipulation
                             nums.Insert(index, int.Parse(cmd[i]));
                         }
                         break;
-                        //vzdgdz
+                        //vzdgdzvbcbkjckjcd
                     //TODO
                     case "print":
                         Console.WriteLine(string.Join(' ', nums));
